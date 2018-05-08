4a6823d0e0dc9972e1330da1bbd5921500ec1eb6
Glen Summers <glen.summers@outlook.com>
2018-05-08 21:34:23 +0100

    draw candles

--- GDaxGui/candlechart.cpp
+++ GDaxGui/candlechart.cpp
@@ -13,6 +13,9 @@ void CandleChart::setCandles(std::vector<Candle> candles)
 
 void CandleChart::paint(QPainter & painter) const
 {
+    // todo, feed in new tick values
+    // add ema
+
     /*
     time_t startTime;
     Decimal lowestPrice;
@@ -28,47 +31,59 @@ void CandleChart::paint(QPainter & painter) const
         return;
     }
 
-    time_t startTime = candles.rbegin()->startTime;
-    time_t endTime = candles.begin()->startTime + 3600;
+    //time_t startTime = candles.rbegin()->startTime;
+    //time_t endTime = candles.begin()->startTime + 60*60;
 
-    double min = std::numeric_limits<double >::max();
-    double max = std::numeric_limits<double >::lowest();
+    time_t startTime = time(nullptr);
+
+    double min = std::numeric_limits<double>::max();
+    double max = std::numeric_limits<double>::lowest();
     for (const auto & c : candles)
     {
         max = std::max(max, c.highestPrice.getAsDouble());
         min = std::min(min, c.lowestPrice.getAsDouble());
     }
 
-    time_t timeRange = endTime - startTime;
+    // mode = 1hr
+    time_t timeDelta = 60*60; //1d init, max = endTime - startTime;
+    float xScale = 500.f/24/60/60; // init 500px per day
     double yRange = (max-min);
+    double wedgie = 0.3*timeDelta*xScale;
 
-    float width = this->width();
-    float height = this->height();
+    int width = this->width();
+    int height = this->height();
 
     QPen penUp(QColor(qRgb(0,255,0)), 2);
+    QBrush brushUp(QColor(qRgb(0,0,0)));
     QPen penDown(QColor(qRgb(255,0,0)), 2);
+    QBrush brushDown(QColor(qRgb(255,0,0)));
 
-    for (const auto & c : candles) // gies back in time...
+    for (const auto & c : candles) // goes back in time...
     {
-        float x = width * (c.startTime - startTime) / timeRange;
+        float x = width - (startTime - c.startTime) * xScale; // from right!
         auto y0 = static_cast<float>(((max-c.lowestPrice.getAsDouble()) / yRange) * height);
         auto y1 = static_cast<float>(((max-c.highestPrice.getAsDouble()) / yRange) * height);
+        auto yy0 = static_cast<float>(((max-c.openingPrice.getAsDouble()) / yRange) * height);
+        auto yy1 = static_cast<float>(((max-c.closingPrice.getAsDouble()) / yRange) * height);
 
         if (c.closingPrice > c.openingPrice)
         {
             painter.setPen(penUp);
-            painter.drawLine(QPointF(x, y0), QPointF(x, y1));
+            painter.setBrush(brushUp);
         }
         else if (c.closingPrice < c.openingPrice)
         {
             painter.setPen(penDown);
-            painter.drawLine(QPointF(x, y0), QPointF(x, y1));
+            painter.setBrush(brushDown);
+        }
+        else
+        {
+            continue;
         }
 
-// if xscale > ...
-//        auto yy0 = static_cast<float>(((c.openingPrice - min) / yRange).getAsDouble() * height);
-//        auto yy1 = static_cast<float>(((c.closingPrice - min) / yRange).getAsDouble() * height);
-//        painter.drawRect(QRectF(x-xDelta/2, yy0, x+xDelta/2, yy1));
+        painter.drawLine(QPointF(x, y0), QPointF(x, y1));
+        // if xscale > ...
+        painter.drawRect(QRectF(QPointF(x-wedgie, yy0), QPointF(x+wedgie, yy1)));
     }
 
 }
